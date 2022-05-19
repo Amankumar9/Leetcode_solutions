601d930f5881e2e3486dd55e7e0a6f44b63b58d1
Aman Kumar <46536038+Amankumar9@users.noreply.github.com>
2022-05-20 03:05:27 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (91.10%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,18 +1,19 @@
-class Solution {
-public:
-    int uniquePaths(int m, int n) {
-        int d[m][n];
-        for(int i=0;i<m;i++)
+class Solution
+{
+    public:
+        int uniquePaths(int m, int n)
         {
-         for(int j=0;j<n;j++)
-         {
-             if(i==0 || j==0)
-                 d[i][j]=1;
-             else
-                 d[i][j]=d[i-1][j]+d[i][j-1];  
-         }
+            int d[m][n];
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == 0 || j == 0)
+                        d[i][j] = 1;
+                    else
+                        d[i][j] = d[i - 1][j] + d[i][j - 1];
+                }
+            }
+            return d[m - 1][n - 1];
         }
-         return d[m-1][n-1];  
-    }
 };
- 

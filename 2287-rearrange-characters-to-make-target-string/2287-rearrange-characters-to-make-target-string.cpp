496c40ebc31ea7d77118a5d19316d87dd84ced81
Aman Kumar <46536038+Amankumar9@users.noreply.github.com>
2022-06-03 17:04:56 +0530

    Time: 3 ms (50.34%), Space: 6.3 MB (32.68%) - LeetHub

--- 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.cpp
+++ 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     int rearrangeCharacters(string s, string target) {
-    unordered_map<char,int>mp1,mp2;
-    for(int i=0;i<s.length();i++)
-    {
-        mp1[s[i]]++;
-    }
-    for(int i=0;i<target.length();i++)
-    {
-        mp2[target[i]]++;
-    }
-    int res=INT_MAX;
-    for(int i=0;i<target.length();i++)
-    {
-        res=min(res,mp1[target[i]]/mp2[target[i]]);
-    }
-    return res;
+        vector<int> vS(26,0);
+        vector<int>vT(26,0);
+        for(char &ch:s)
+            vS[ch-'a']++;
+        for(char &ch:target)
+            vT[ch-'a']++;
+        int ans=INT_MAX;
+        for(int i=0;i<26;i++){
+            if(vT[i]>0)
+                ans=min(ans,vS[i]/vT[i]);
+        }
+        return ans;
   }
 };

cc07bd4056b3e849cc16dbd1fb8f85311b6cc256
Aman Kumar <46536038+Amankumar9@users.noreply.github.com>
2022-06-03 17:06:06 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (88.62%) - LeetHub

--- 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.cpp
+++ 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.cpp
@@ -3,10 +3,10 @@ public:
     int rearrangeCharacters(string s, string target) {
         vector<int> vS(26,0);
         vector<int>vT(26,0);
-        for(char &ch:s)
-            vS[ch-'a']++;
-        for(char &ch:target)
-            vT[ch-'a']++;
+        for(int i=0;i<s.length();i++)
+            vS[s[i]-'a']++;
+        for(int i=0;i<target.length();i++)
+            vT[target[i]-'a']++;
         int ans=INT_MAX;
         for(int i=0;i<26;i++){
             if(vT[i]>0)

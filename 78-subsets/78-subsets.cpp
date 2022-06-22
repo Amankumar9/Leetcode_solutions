cff532c79896b6820b18f25d81b371a77c7eb018
Aman Kumar <46536038+Amankumar9@users.noreply.github.com>
2022-06-23 02:02:39 +0530

    Time: 3 ms (71.07%), Space: 15.8 MB (25.82%) - LeetHub

---
+++ 78-subsets/78-subsets.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>res;
+        vector<int>curr;
+        help(0,nums,curr,res);
+        return res;
+    }
+    
+    void help(int i, vector<int>&nums, vector<int>curr, vector<vector<int>>& res)
+    {
+        if(i==nums.size())
+        {
+            res.push_back(curr);
+            return;
+        }
+        else
+        {
+            // include ith ele
+            curr.push_back(nums[i]);
+            help(i+1,nums,curr,res);
+            curr.pop_back();
+            // exclude ith ele
+            help(i+1,nums,curr,res);
+        }
+    }
+    
+};

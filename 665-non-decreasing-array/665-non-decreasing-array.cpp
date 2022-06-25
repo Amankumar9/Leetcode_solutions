e2890afe3b7d05f50c87fbef43e9c7a5e77a1e2f
Aman Kumar <46536038+Amankumar9@users.noreply.github.com>
2022-06-25 15:29:03 +0530

    Time: 50 ms (25.96%), Space: 26.9 MB (81.98%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.cpp
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -19,7 +19,8 @@ public:
                     cnt++;
                  }
              }
-            
+             if(cnt>1)
+                return false;
         }
         return cnt<=1;
     }

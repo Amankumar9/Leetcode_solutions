d5006450296b1228d1b5bae12cbc45d088af561a
Aman Kumar <46536038+Amankumar9@users.noreply.github.com>
2022-07-09 18:47:33 +0530

    Time: 23 ms (98.96%), Space: 11.6 MB (34.32%) - LeetHub

--- 1433-check-if-a-string-can-break-another-string/1433-check-if-a-string-can-break-another-string.cpp
+++ 1433-check-if-a-string-can-break-another-string/1433-check-if-a-string-can-break-another-string.cpp
@@ -8,7 +8,7 @@ public:
         for(int i=0;i<n2;i++)
             fr2[s2[i]-'a']++;
         bool fl1=true,fl2=true;
-        for(int i=25;i>=0;i--)
+        for(int i=0;i<25;i++)
         {
             cnt1+=(fr1[i]-fr2[i]);
             cnt2+=(fr2[i]-fr1[i]);

184fa708529b2f53954f426ad7c4b37bbfe8ee49
Aman Kumar <46536038+Amankumar9@users.noreply.github.com>
2022-07-09 18:46:30 +0530

    Time: 31 ms (94.45%), Space: 11.6 MB (34.32%) - LeetHub

--- 1433-check-if-a-string-can-break-another-string/1433-check-if-a-string-can-break-another-string.cpp
+++ 1433-check-if-a-string-can-break-another-string/1433-check-if-a-string-can-break-another-string.cpp
@@ -2,16 +2,21 @@ class Solution {
 public:
     bool checkIfCanBreak(string s1, string s2) {
         int n1=s1.size(),n2=s2.size();
-        sort(s1.begin(),s1.end());
-        sort(s2.begin(),s2.end());
-        int cnt1=0,cnt2=0;
+        int fr1[26]={0},fr2[26]={0},cnt1=0,cnt2=0;;
         for(int i=0;i<n1;i++)
+            fr1[s1[i]-'a']++;
+        for(int i=0;i<n2;i++)
+            fr2[s2[i]-'a']++;
+        bool fl1=true,fl2=true;
+        for(int i=25;i>=0;i--)
         {
-            if(s1[i]>s2[i])
-                cnt1++;
-            if(s2[i]>s1[i])
-                cnt2++;
+            cnt1+=(fr1[i]-fr2[i]);
+            cnt2+=(fr2[i]-fr1[i]);
+            if(cnt1<0)
+                fl1=false;
+            if(cnt2<0)
+                fl2=false;
         }
-        return (cnt1==0 || cnt2==0);
+        return (fl1 || fl2); 
     }
 };

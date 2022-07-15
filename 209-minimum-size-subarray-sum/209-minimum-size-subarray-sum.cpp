97797012bc18d456014d3f8d6b44cd4f06cce6ca
Aman Kumar <46536038+Amankumar9@users.noreply.github.com>
2022-07-15 15:15:11 +0530

    Time: 49 ms (34.14%), Space: 28.2 MB (20.91%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int minSubArrayLen(int tar, vector<int>& nums) {
-        int n=nums.size(),lft=0,res=INT_MAX,cursum=0,curcnt=0;
+        int n=nums.size(),lft=0,res=INT_MAX,cursum=0;
         for(int i=0;i<n;i++)
         {
             cursum+=nums[i];

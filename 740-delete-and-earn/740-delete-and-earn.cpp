8d2d018577de199767ae6cbb9c74f67b28abb04a
Aman Kumar <46536038+Amankumar9@users.noreply.github.com>
2022-07-16 23:30:36 +0530

    Time: 27 ms (41.71%), Space: 16.2 MB (26.78%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -7,12 +7,12 @@ public:
            int number=nums[i]+v[nums[i]];
            v[nums[i]]=number;
        }
-       int res=0;
        if(v.size()==1)
            return v[0];
        if(v.size()==2)
            return max(v[0],v[1]);
        v[1]=max(v[0],v[1]);
+       int res=0;
        for(int i=2;i<v.size();i++)
        {
            v[i]=max(v[i-2]+v[i],v[i-1]);

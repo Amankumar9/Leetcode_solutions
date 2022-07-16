4f599c6831935011716b536a814e2dbca739198c
Aman Kumar <46536038+Amankumar9@users.noreply.github.com>
2022-07-16 23:26:24 +0530

    Time: 24 ms (50.79%), Space: 16.2 MB (26.78%) - LeetHub

---
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int deleteAndEarn(vector<int>& nums) {
+       vector<int>v(20001,0);
+       for(int i=0;i<nums.size();i++)
+       {
+           int number=nums[i]+v[nums[i]];
+           v[nums[i]]=number;
+       }
+       int res=0;
+       if(v.size()==1)
+           return v[0];
+       if(v.size()==2)
+           return max(v[0],v[1]);
+       v[1]=max(v[0],v[1]);
+       for(int i=2;i<v.size();i++)
+       {
+           v[i]=max(v[i-2]+v[i],v[i-1]);
+       }
+       return v[v.size()-1];
+    }
+};

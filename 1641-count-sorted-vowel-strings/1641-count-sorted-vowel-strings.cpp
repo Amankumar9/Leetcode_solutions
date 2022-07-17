9571dec15e2e076a29fa18ff6f3db20c0810cf55
Aman Kumar <46536038+Amankumar9@users.noreply.github.com>
2022-07-18 00:15:14 +0530

    Time: 3 ms (48.84%), Space: 6 MB (34.76%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -4,18 +4,12 @@ public:
         if(n==1)
             return 5;
         int res=0;
-        vector<vector<int>>dp(n,vector<int>(5,1));
-        for(int i=1;i<n;i++)
+        vector<int>dp(5,1);
+        for(int i=1;i<=n;i++)
         {
-            int cur=0;
-            for(int j=4;j>=0;j--)
-            {
-                cur+=dp[i-1][j];
-                dp[i][j]=cur;
-            }
+            for(int j=3;j>=0;j--)
+                dp[j]+=dp[j+1];
         }
-        for(int j=0;j<4;j++)
-            res+=dp[n-1][j];
-        return res+1;
+        return dp[0];
     }
 };

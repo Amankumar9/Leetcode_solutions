f1966687841e56d186a16331991e852149233445
Aman Kumar <46536038+Amankumar9@users.noreply.github.com>
2022-08-14 04:30:55 +0530

    Time: 175 ms (69.22%), Space: 29 MB (67.53%) - LeetHub

---
+++ 2186-minimum-number-of-steps-to-make-two-strings-anagram-ii/2186-minimum-number-of-steps-to-make-two-strings-anagram-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minSteps(string s, string t) {
+        vector<int>mp1(26,0),mp2(26,0);
+        int res=0;
+        for(int i=0;i<s.size();i++)
+        {
+            mp1[s[i]-'a']++;
+        }
+        for(int i=0;i<t.size();i++)
+        {
+            mp2[t[i]-'a']++;
+        }
+        for(int i=0;i<26;i++)
+        {
+            res+=abs(mp1[i]-mp2[i]);
+        }
+        return res;
+    }
+};

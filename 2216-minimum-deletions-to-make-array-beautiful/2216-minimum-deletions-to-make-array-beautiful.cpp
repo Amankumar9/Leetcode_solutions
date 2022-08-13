b8b5706f03caf6f17d4fd729822fffb8fbdf09b2
Aman Kumar <46536038+Amankumar9@users.noreply.github.com>
2022-08-13 08:55:32 +0530

    Time: 291 ms (52.67%), Space: 121.1 MB (93.18%) - LeetHub

--- 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.cpp
+++ 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.cpp
@@ -2,15 +2,11 @@ class Solution {
 public:
     int minDeletion(vector<int>& nums) {
         int n=nums.size(),res=0;
-        stack<pair<int,int>>st;
-        st.push({nums[0],0});
-        for(int i=1;i<n;i++)
+        for(int i=0;i<n-1;i++)
         {
-            if(st.top().first==nums[i] && st.top().second%2==0)
+            if(nums[i]==nums[i+1] && (i-res)%2==0)
                 res++;
-            else
-                st.push({nums[i],st.top().second+1});
         }
-        return st.size()%2==0?res:res+1;
+        return res+(n-res)%2;
     }
 };

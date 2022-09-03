94424d1c30fa7bbc283ae50ede73a108c7fac750
Aman Kumar <46536038+Amankumar9@users.noreply.github.com>
2022-09-04 01:56:12 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (78.41%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -3,12 +3,12 @@ public:
     int fib(int n) {
         if(n<2)
             return n;
-        int a=0,b=1;
+        int a=0,b=1,c;
         for(int i=2;i<=n;i++){
-            int c=a+b;
+            c=a+b;
             a=b;
             b=c;
         }
-        return b;
+        return c;
     }
 };
